4b5614bd88642934980d6ed578fd789da86cae42
Target <71287126+Flroa@users.noreply.github.com>
2024-05-28 16:14:14 +0200

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <meta name="color-scheme" content="light dark">
+        <style>undefined</style>
+        <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin="use-credentials">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Mulish:wght@200;300;400;500;600;700;800;900&amp;display=swa">
+        <style>
+            body {
+                background-color: white;
+            }
+            
+            pre {
+                color: white;
+            }
+            
+            body::selection,
+            pre::selection {
+                background-color: transparent;
+                color: inherit;
+            }
+            
+            body::-moz-selection,
+            pre::-moz-selection {
+                background-color: transparent;
+                color: inherit;
+            }
+        </style>
+    </head>
+    <body>
+        <pre style="word-wrap: break-word; white-space: pre-wrap;">
+            print("kys")
+        </pre>
+    </body>
+</html>
